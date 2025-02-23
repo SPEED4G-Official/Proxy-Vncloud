de1a374a9fb0036dc770d86b4c60ff5d373d2e22
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2025-02-24 00:46:16 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,38 +1,34 @@
 #!/bin/bash
-
-./ioncube_encoder.sh -C -x86-64 -81 filemahoa -o mahoadone
+version="$1"
+./ioncube_encoder.sh -C -x86-64 -$version filemahoa -o mahoadone
+cd mahoadone
+ZIP_NAME="mahoa_$(date +%Y%m%d%H%M%S).zip"
+zip -r "$ZIP_NAME"
 getServer() {
     response=$(curl -s "https://api.gofile.io/servers")
     echo $(jq -r '.data.servers[0].name' <<< "$response")
 }
 
-# Định nghĩa biến
-TOKEN="<ĐƯỜNG_DẪN_TOKEN_CỦA_BẠN>"  # Nếu cần token
-FILE_NAME="test.zip"  # Đổi thành tên file cần upload
-ZIP_FILE_PATH="${TOKEN}/${FILE_NAME}"
+ZIP_FILE_PATH="${ZIP_NAME}"
 
-# Lấy server khả dụng từ Gofile
+
 SERVER=$(getServer)
 
-# Kiểm tra nếu server không lấy được
 if [ -z "$SERVER" ]; then
-    echo "Không thể lấy server từ Gofile!"
+    echo "Không Thể Lấy Server Từ Gofile!"
     exit 1
 fi
 
-# Upload file lên Gofile
 RESPONSE=$(curl -s -X POST "https://${SERVER}.gofile.io/contents/uploadfile" \
     -F "file=@${ZIP_FILE_PATH}")
 
-# In phản hồi từ API
-echo "Phản hồi từ Gofile: $RESPONSE"
-
-# Lấy link tải file từ phản hồi API
 DOWNLOAD_LINK=$(jq -r '.data.downloadPage' <<< "$RESPONSE")
 
 if [ "$DOWNLOAD_LINK" != "null" ]; then
-    echo "File đã được upload thành công!"
-    echo "Link tải: $DOWNLOAD_LINK"
+    cd ..
+    rm -rf mahoadone
+    echo "File Đã Được Mã Hóa Thành Công!"
+    echo "Link Tải: $DOWNLOAD_LINK"
 else
-    echo "Upload thất bại!"
+    echo "Upload Thất Bại!"
 fi
